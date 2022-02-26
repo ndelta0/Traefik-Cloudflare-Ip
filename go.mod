4a85a1530a6adc3404a45bfa2c9d293d3e5c0f21
Piotr Madura <ppiotr.madura@gmail.com>
2022-02-26 19:46:48 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ndelta0/Traefik-Cloudflare-Ip
+
+go 1.17
---
+++ plugin.go
@@ -0,0 +1,46 @@
+package traefikCloudflareIp
+
+import (
+	"context"
+	"net/http"
+)
+
+type Config struct {
+}
+
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+type Plugin struct {
+	next http.Handler
+	name string
+}
+
+func (p Plugin) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	trueIp := request.Header.Get("cf-connecting-ip")
+	if len(trueIp) > 0 {
+		request.Header.Set("x-real-ip", trueIp)
+
+		var trueForwarded string
+		forwarded := request.Header.Get("x-forwarded-for")
+		if len(forwarded) > 0 {
+			trueForwarded = trueIp + "," + forwarded
+		} else {
+			trueForwarded = trueIp
+		}
+
+		request.Header.Set("x-forwarded-for", trueForwarded)
+	}
+
+	p.next.ServeHTTP(writer, request)
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	plugin := &Plugin{
+		next: next,
+		name: name,
+	}
+
+	return plugin, nil
+}
